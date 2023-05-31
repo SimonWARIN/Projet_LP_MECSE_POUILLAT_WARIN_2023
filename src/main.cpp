ad2135f777ea604cb104a0149911165a51a11aba
SimonWARIN <sim.warin@gmail.com>
2023-05-31 11:08:51 +0200

    test

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+const int channel = 0;         // Canal PWM à utiliser
+const int frequency = 50;    // Fréquence PWM en Hz
+const int resolution = 8;      // Résolution PWM en bits (8 bits pour une sortie allant de 0 à 255)
+
+const int pwmPin = 13;         // Broche de sortie PWM
+
+void setup() {
+  // Configuration de la bibliothèque ledc
+  ledcSetup(channel, frequency, resolution);
+  ledcAttachPin(pwmPin, channel);
+}
+
+void loop() {
+  // Variation de la valeur du signal PWM
+  
+  // Augmentation progressive de la valeur
+    ledcWrite(channel, 25);
+    delay(1000);
+    ledcWrite(channel, 12);
+    delay(1000);
+}
